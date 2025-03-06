b9476cba4e38de59d61efe13dc2c62f05888a207
ViktorGox <viktor.krastev03@gmail.com>
2025-03-06 16:43:21 +0100

    Added classes related to the user end points.

---
+++ src/main/java/aad/message/app/user/UserController.java
@@ -0,0 +1,22 @@
+package aad.message.app.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository repository;
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return repository.findAll();
+    }
+}
+
