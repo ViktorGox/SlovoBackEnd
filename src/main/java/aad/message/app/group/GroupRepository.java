8ad5d20f26b46a3ba3c70544e593ebef8c4a8de9
Borislav Markov <533390@student.saxion.nl>
2025-03-08 22:23:33 +0100

    B-4 Added repositories for group and group_user

---
+++ src/main/java/aad/message/app/group/GroupRepository.java
@@ -0,0 +1,6 @@
+package aad.message.app.group;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<Group, Long> {
+}
---
+++ src/main/java/aad/message/app/group_user/GroupUserRepository.java
@@ -0,0 +1,6 @@
+package aad.message.app.group_user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
+}
