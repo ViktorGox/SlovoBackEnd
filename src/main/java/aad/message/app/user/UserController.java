21d105a453c6b52d2813e64f1327e0f3f0b4fb3d
ViktorGox <viktor.krastev03@gmail.com>
2025-03-13 11:23:04 +0100

    Added a todo.

--- src/main/java/aad/message/app/user/UserController.java
+++ src/main/java/aad/message/app/user/UserController.java
@@ -64,6 +64,7 @@ public class UserController {
     }
 
     // TODO: Didn't delete the old image once, couldn't replicate it afterwards though.
+    // TODO: Handle providing duplicate email, check and return already in use, now it does 401
     @PutMapping
     public ResponseEntity<?> update(@RequestPart(value = "file", required = false) MultipartFile file,
                                     @RequestPart(value = "dto", required = false) UserUpdateDTO dto) {
