c24cbf1846485f172ae1cdb7123a534ff42e1d9f
ViktorGox <viktor.krastev03@gmail.com>
2025-03-14 10:30:49 +0100

    Added DTO for the /POST messageAudio

---
+++ src/main/java/aad/message/app/messageaudio/MessageAudioPostDTO.java
@@ -0,0 +1,9 @@
+package aad.message.app.messageaudio;
+
+import java.util.List;
+
+public class MessageAudioPostDTO {
+    public Long id;
+    public Long replyMessageId;
+    public List<Long> groupIds;
+}
