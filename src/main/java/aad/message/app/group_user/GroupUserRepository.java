ca49517bf69e07c97062be32ddb107fc71297695
Borislav Markov <533390@student.saxion.nl>
2025-03-17 21:48:17 +0100

    B-7 Added middleware to check if user is part of the group

--- src/main/java/aad/message/app/group_user/GroupUserRepository.java
+++ src/main/java/aad/message/app/group_user/GroupUserRepository.java
@@ -6,4 +6,5 @@ import java.util.List;
 
 public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
     List<GroupUser> findByGroupId(Long groupId);
+    boolean existsByUserIdAndGroupId(Long userId, Long groupId);
 }
