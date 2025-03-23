e7735d69a60f8b3d6689e29f35f3fa1fd20176fe
Borislav Markov <533390@student.saxion.nl>
2025-03-23 16:19:02 +0100

    B-15 Added additional role classes

---
+++ src/main/java/aad/message/app/role/RoleController.java
@@ -0,0 +1,22 @@
+package aad.message.app.role;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/roles")
+public class RoleController {
+    private final RoleService roleService;
+
+    public RoleController(RoleService roleService) {
+        this.roleService = roleService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+}
