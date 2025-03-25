aa9faca5d968c73a760f1b6a516edc54deb2de9c
Borislav Markov <533390@student.saxion.nl>
2025-03-25 18:02:57 +0100

    B-18 Fixed issue with interceptor

---
+++ src/main/java/aad/message/app/group/RecentChatDTO.java
@@ -0,0 +1,10 @@
+package aad.message.app.group;
+
+import aad.message.app.message.Message;
+
+public class RecentChatDTO {
+    public String groupTitle;
+    public String groupImage;
+    public Message lastUserMessage;
+    public Message lastMessage;
+}
