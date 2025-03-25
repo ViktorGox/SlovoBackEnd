508c917237bd6a0346104bbc64694aff7416bc47
Borislav Markov <533390@student.saxion.nl>
2025-03-25 17:54:55 +0100

    B-18 Created queries to get latest messages

--- src/main/java/aad/message/app/message/messageaudio/MessageAudioRepository.java
+++ src/main/java/aad/message/app/message/messageaudio/MessageAudioRepository.java
@@ -5,8 +5,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageAudioRepository extends JpaRepository<MessageAudio, Long> {
     @Query("SELECT ma FROM MessageAudio ma JOIN ma.groups g WHERE g.id = :groupId")
     List<MessageAudio> findByGroupId(@Param("groupId") Long groupId);
+    @Query("SELECT ma FROM MessageAudio ma " +
+            "JOIN MessageAudioGroup mag ON ma.id = mag.messageAudio.id " +
+            "JOIN mag.group g " +
+            "WHERE g.id = :groupId AND mag.messageAudio.user.id = :userId " +
+            "ORDER BY ma.sentDate DESC")
+    Optional<MessageAudio> findTopByUserIdAndGroupIdOrderBySentDateDesc(@Param("userId") Long userId,
+                                                                        @Param("groupId") Long groupId);
 }
+
