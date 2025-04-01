3f7dbc574d4734d221dfd63d0090a3134adbba83
ViktorGox <viktor.krastev03@gmail.com>
2025-04-01 17:38:13 +0200

    Changed the get messages end point to require a page number in addition, will return 15 messages, or that page.

--- src/main/java/aad/message/app/message/MessageRepository.java
+++ src/main/java/aad/message/app/message/MessageRepository.java
@@ -1,10 +1,11 @@
 package aad.message.app.message;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
@@ -16,7 +17,6 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
                 LEFT JOIN MessageAudio ma ON m.id = ma.id 
                 LEFT JOIN ma.groups g 
                 WHERE mt.group.id = :groupId OR g.id = :groupId
-                ORDER BY m.sentDate
             """)
-    List<Message> getMessagesByGroupId(@Param("groupId") Long groupId);
+    Page<Message> getMessagesByGroupId(@Param("groupId") Long groupId, Pageable pageable);
 }
