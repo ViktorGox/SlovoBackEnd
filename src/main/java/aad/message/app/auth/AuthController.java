e7394165118ade25c743f64e9695214ed619aa59
Borislav Markov <533390@student.saxion.nl>
2025-04-01 18:17:48 +0200

    B-19 Refresh token is now returned during login

--- src/main/java/aad/message/app/auth/AuthController.java
+++ src/main/java/aad/message/app/auth/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -40,6 +41,14 @@ public class AuthController {
                     .body(Collections.singletonMap("error", "Invalid username or password"));
         }
 
-        return ResponseEntity.ok().body(Collections.singletonMap("token", jwtUtils.generateAccessToken(user.get().id)));
+        String accessToken = jwtUtils.generateAccessToken(user.get().id);
+        String refreshToken = jwtUtils.generateRefreshToken(user.get());
+
+        // Return both tokens in the response
+        return ResponseEntity.ok()
+                .body(Map.of(
+                        "accessToken", accessToken,
+                        "refreshToken", refreshToken
+                ));
     }
 }
