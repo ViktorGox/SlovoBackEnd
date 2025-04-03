cd4a9370ade594d4f75f0810c6214baec2b1fe26
ViktorGox <viktor.krastev03@gmail.com>
2025-04-03 23:08:03 +0200

    Added groupId to the RecentChatDTO

--- src/main/java/aad/message/app/group/RecentChatDTO.java
+++ src/main/java/aad/message/app/group/RecentChatDTO.java
@@ -5,6 +5,7 @@ import aad.message.app.message.RecentMessageDTO;
 import java.time.LocalDateTime;
 
 public class RecentChatDTO {
+    public Long groupId;
     public String groupTitle;
     public String groupImage;
     public LocalDateTime lastUserMessageTime;
